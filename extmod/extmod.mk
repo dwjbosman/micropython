17b7c4745b0af529f9b8e6605da7e8b17d697ee1
Dinne Bosman <dwjbosman@yahoo.com>
2019-10-20 18:31:58 +0200

    Enable custom module

--- extmod/extmod.mk
+++ extmod/extmod.mk
@@ -191,8 +191,9 @@ $(BUILD)/$(BTREE_DIR)/%.o: CFLAGS += -Wno-old-style-definition -Wno-sign-compare
 $(BUILD)/extmod/modbtree.o: CFLAGS += $(BTREE_DEFS)
 endif
 
+ifeq ($(MICROPY_PY_CUSTOM),1)
 INC += -I$(BOARD_DIR)
 #SRC_CPP = extmod/custommodule/custommodule.cpp
 PY_O += extmod/custommodule/custommodule.o
 PY_O += $(addprefix $(BUILD)//, $(SRC_CPP:.cpp=.o))
-
+endif
